c9fa5c37fabcf229675195700b5e37ef353780cc
vivek.v <paivivek002@gmail.com>
2023-12-23 15:40:58 +0530

    program one completed

--- program_1/src/app/app.component.ts
+++ program_1/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'program_1';
+
+  form: FormGroup = new FormGroup({});
+
+  first_name: any = '';
+  last_name: any = '';
+
+  alert: boolean = false;
+
+  constructor() {
+    this.form = new FormGroup({
+      first_name: new FormControl('', [Validators.required]),
+      last_name: new FormControl('', [Validators.required]),
+    });
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      console.log('ERROR');
+      this.alert = true;
+      this.first_name = '';
+      this.last_name = '';
+    } else {
+      console.log(this.form.value);
+      this.first_name = this.form.value.first_name;
+      this.last_name = this.form.value.last_name;
+    }
+  }
 }
--- program_1/src/app/app.module.ts
+++ program_1/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
