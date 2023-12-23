1fcf12002bfc21a7dac869358b34e1106fa891e5
vivek.v <paivivek002@gmail.com>
2023-12-23 16:13:11 +0530

    pg 2 complete

--- program_2/src/app/app.component.ts
+++ program_2/src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'program_2';
+  item_to_add: any = { name: '', quantity: '', price: '' };
+
+  shopping_list: any = [
+    { name: 'Item 1', quantity: 1, price: 100 },
+    { name: 'Item 2', quantity: 2, price: 200 },
+    { name: 'Item 3', quantity: 3, price: 300 },
+  ];
+
+  add() {
+    console.log(this.item_to_add);
+    this.shopping_list.unshift(this.item_to_add);
+  }
+
+  delete(index:any){
+    this.shopping_list.splice(index, 1);
+  }
 }
