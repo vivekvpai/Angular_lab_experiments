3e5b16453aabec99e5ee30352f2ea5e9633fca28
vivek.v <paivivek002@gmail.com>
2023-12-24 10:28:37 +0530

    pg 3 added

--- program_3/src/app/app.component.ts
+++ program_3/src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'program_3';
+
+  number_1: number = 0;
+  number_2: number = 0;
+  result: number = 0;
+
+  get_result(operation: string) {
+    switch (operation) {
+      case 'add':
+        this.result = this.number_1 + this.number_2;
+        break;
+      case 'sub':
+        this.result = this.number_1 - this.number_2;
+        break;
+      case 'multiply':
+        this.result = this.number_1 * this.number_2;
+        break;
+      case 'divide':
+        this.result = this.number_1 / this.number_2;
+        break;
+
+      default:
+        this.result = 0;
+        break;
+    }
+  }
 }
--- program_3/src/app/app.module.ts
+++ program_3/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
