39ef0010f12ad379804316f883c853d9d61bea22
vivek.v <paivivek002@gmail.com>
2023-12-24 11:48:09 +0530

    pg 4 added

--- program_4/src/app/app.component.ts
+++ program_4/src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'program_4';
+
+  inputNumber = '';
+  factorialResult = '';
+  squareResult = '';
+
+  calculateFactorial() {
+    const number = parseInt(this.inputNumber);
+    if (Number.isInteger(number) && number >= 0) {
+      let result = 1;
+      for (let i = 2; i <= number; i++) {
+        result *= i;
+      }
+      this.factorialResult = result.toString();
+    } else {
+      this.factorialResult = 'Invalid input';
+    }
+  }
+
+  calculateSquare() {
+    const number = parseFloat(this.inputNumber);
+    if (!isNaN(number)) {
+      this.squareResult = (number * number).toString();
+    } else {
+      this.squareResult = 'Invalid input';
+    }
+  }
 }
--- program_4/src/app/app.module.ts
+++ program_4/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
