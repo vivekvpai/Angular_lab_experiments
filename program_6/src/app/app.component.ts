2cdb64a0a426ee3b9dc383569182c8dc9af73a2f
vivek.v <paivivek002@gmail.com>
2024-01-15 13:07:15 +0530

    pg 6 added

--- program_6/src/app/app.component.ts
+++ program_6/src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'program_6';
+
+  item_to_add: any = {
+    index: 0,
+    to_do: '',
+  };
+
+  to_do_list: any = [
+    { index: 0, to_do: 'Take Tablets' },
+    { index: 1, to_do: 'Bring Milk' },
+  ];
+
+  is_edit: boolean = false;
+
+  add() {
+    this.item_to_add.index = this.to_do_list.length;
+    this.to_do_list.push(this.item_to_add);
+  }
+
+  remove(index: any) {
+    this.to_do_list.splice(index, 1);
+  }
+
+  edit(index: any) {
+    this.is_edit = true;
+  }
 }
