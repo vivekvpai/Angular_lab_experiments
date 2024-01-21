18a0d29a631ca80bd99b743d0d5d70c07f898469
vivek.v <vivek.v@metricdust.com>
2024-01-21 11:38:36 +0530

    filter added

--- program_9/src/app/app.component.ts
+++ program_9/src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'program_9';
+  search: any = '';
+
+  employee_list: any = [
+    {
+      name: 'Employee 1',
+      salary: 1000,
+    },
+    {
+      name: 'Employee 2',
+      salary: 1500,
+    },
+    {
+      name: 'Employee 3',
+      salary: 3000,
+    },
+  ];
 }
--- program_9/src/app/app.module.ts
+++ program_9/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FilterPipe],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
