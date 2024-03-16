ce0740adab9e35c30931dd87d0d5d06704568319
vivek-metricdust <vivek.v@metricdust.com>
2024-03-16 23:58:23 +0530

    removed helper

--- program_5/src/app/app.component.ts
+++ program_5/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { student_data } from './helpers/student';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +8,41 @@ import { student_data } from './helpers/student';
 export class AppComponent {
   title = 'program_5';
 
-  student_list: any = student_data;
+  student_list: any = [
+    {
+      name: 'Student_1',
+      semester_1: 8.1,
+      semester_2: 8.2,
+      semester_3: 8.3,
+      semester_4: 8.4,
+      semester_5: 8.5,
+      semester_6: 8.6,
+      semester_7: 8.7,
+      semester_8: 8.8,
+    },
+    {
+      name: 'Student_2',
+      semester_1: 9.1,
+      semester_2: 9.2,
+      semester_3: 9.3,
+      semester_4: 9.4,
+      semester_5: 9.5,
+      semester_6: 9.6,
+      semester_7: 9.7,
+      semester_8: 9.8,
+    },
+    {
+      name: 'Student_3',
+      semester_1: 7.7,
+      semester_2: 7.8,
+      semester_3: 7.9,
+      semester_4: 8.7,
+      semester_5: 8.8,
+      semester_6: 8.9,
+      semester_7: 9.1,
+      semester_8: 9.2,
+    },
+  ];
 
   get_cgpa(item: any) {
     let result =
